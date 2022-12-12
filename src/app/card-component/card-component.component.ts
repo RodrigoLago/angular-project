d21fa8c41d5d24b1d99dd5b6fd713e88e159108a
RodrigoLago <rodrigazo98@gmail.com>
2022-12-12 00:46:38 -0300

    Change style

--- src/app/card-component/card-component.component.ts
+++ src/app/card-component/card-component.component.ts
@@ -10,11 +10,13 @@ export class CardComponentComponent implements OnInit{
   @Input()title:String;
   @Input()description:String;
   @Input()card_image:String;
- 
+  @Input()writer:String;
+
   constructor(){
     this.title = "Titulo generico";
     this.description = "Descripcion generica"
     this.card_image = "Imagen generica"
+    this.writer = "Nombre de escritor generico"
    }
    ngOnInit(): void {
      
--- src/app/card-container/card-container.component.ts
+++ src/app/card-container/card-container.component.ts
@@ -4,6 +4,7 @@ interface Carta{
   image:string;
   title:string;
   description:string;
+  author:string;
 }
 
 @Component({
@@ -19,11 +20,13 @@ export class CardContainerComponent implements OnInit{
   
   ngOnInit(): void{
     this.ArrayCards =[
-      {title:"Titulo 1",description:"Primera descripcion",image:"Image1 src"},
-      {title:"Titulo 2",description:"Segunda descripcion",image:"Image2 src"},
-      {title:"Titulo 3",description:"Tercera descripcion",image:"Image3 src"},
-      {title:"Titulo 4",description:"Cuarta descripcion",image:"Image4 src"},
-      {title:"Titulo 5",description:"Quinta descripcion",image:"Image5 src"}
+      {title:"Titulo 1",description:"Primera descripcion",image:"Image1 src",author:"Pepe"},
+      {title:"Titulo 2",description:"Segunda descripcion",image:"Image2 src",author:"Rodolfo"},
+      {title:"Titulo 3",description:"Tercera descripcion",image:"Image3 src",author:"Pepe"},
+      {title:"Titulo 4",description:"Cuarta descripcion",image:"Image4 src",author:"Juan"},
+      {title:"Titulo 5",description:"Quinta descripcion",image:"Image5 src",author:"Rodolfo"},
+      {title:"Titulo 6",description:"Sexta descripcion",image:"Image6 src",author:"Pepe"},
+
     ]
   }
 }
